2c0389276f567c9c4bb0b2e8a340846cf1b99d00
Florian Koning <florian.koning2004@gmail.com>
2022-01-31 16:29:14 +0100

    Search van Navbar weggehaald
    heb de search bij de Navbar van index.php weg gehaald, die zat daar nog

--- garageproject-1.5/php/index.php
+++ garageproject-1.5/php/index.php
@@ -71,10 +71,6 @@ session_start();
           <a class="nav-link nav-color" href="login.php">Log in</a>
         </li>
       </ul>
-      <form class="d-flex">
-        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-        <button class="btn btn-outline-success" type="submit">Search</button>
-      </form>
     </div>
   </div>
 </nav>
